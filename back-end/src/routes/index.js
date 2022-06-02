aed37ed184daaff5a2429b642d95927a32cdcaa7
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-06-02 16:26:13 -0300

    feat: sales endpoint create and readone and update status

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,12 @@ const loginRoute = require('./login.routes');
 const registerRoute = require('./register.routes');
 const productsRoute = require('./products.routes');
 const sellersRoute = require('./sellers.routes');
+const salesRoute = require('./sales.route');
 
 module.exports = {
   loginRoute,
   registerRoute,
   productsRoute,
   sellersRoute,
+  salesRoute,
 };

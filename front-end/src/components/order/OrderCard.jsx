994c36648b07fa2e59e55de19fcd711dd27f0dfc
Samuel Pereira <gmr.quest.10@gmail.com>
2022-06-08 17:39:45 -0300

    feat: Adiciona data-testid ao OrderCard

--- front-end/src/components/order/OrderCard.jsx
+++ front-end/src/components/order/OrderCard.jsx
@@ -5,33 +5,64 @@ import './order.css';
 function OrderCard({ role }) {
   return (
     <div className="order-card flex">
-      <div className="order-number flex">Pedido 0001</div>
+      <div
+        className="order-number flex"
+        data-testid="customer_orders__element-order-id-ID"
+      >
+        Pedido 0001
+      </div>
 
       {role === 'seller'
         ? (
           <div className="flex group-2-order">
             <div className="flex group-2-a">
               <div className="order-status flex">
-                <h2>Entregue</h2>
+                <h2 data-testid="seller_orders__element-delivery-status-ID">Entregue</h2>
               </div>
 
               <div className="order-details flex">
-                <span className="flex">06/06/2022</span>
-                <span className="flex">R$ 35,00</span>
+                <span
+                  className="flex"
+                  data-testid="seller_orders__element-order-date-ID"
+                >
+                  06/06/2022
+                </span>
+                <span
+                  className="flex"
+                  data-testid="seller_orders__element-card-price-ID"
+                >
+                  R$ 35,00
+                </span>
               </div>
             </div>
 
-            <div className="flex address">Rua Lorem ipsum dolor, nº 100</div>
+            <div
+              className="flex address"
+              data-testid="seller_orders__element-card-address-ID"
+            >
+              Rua Lorem ipsum dolor, nº 100
+            </div>
+
           </div>
         ) : (
           <>
             <div className="order-status flex stretch">
-              <h2>Entregue</h2>
+              <h2 data-testid="customer_orders__element-delivery-status-ID">Entregue</h2>
             </div>
 
             <div className="order-details flex">
-              <span className="flex">06/06/2022</span>
-              <span className="flex">R$ 35,00</span>
+              <span
+                className="flex"
+                data-testid="customer_orders__element-order-date-ID"
+              >
+                06/06/2022
+              </span>
+              <span
+                className="flex"
+                data-testid="customer_orders__element-order-date-ID"
+              >
+                R$ 35,00
+              </span>
             </div>
           </>
         )}

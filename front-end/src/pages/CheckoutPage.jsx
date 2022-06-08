1555dc6bd2b2d41031dfe57b5288d919e3e0f353
David Souza <dabcs@poli.ufrj.br>
2022-06-08 17:59:48 -0300

    feat: cria checkout page.

---
+++ front-end/src/pages/CheckoutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function CheckoutPage() {
+  return (
+    <div>
+      CheckoutPage
+    </div>
+  );
+}
+
+export default connect(null, null)(CheckoutPage);

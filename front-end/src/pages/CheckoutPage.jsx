95efc4f8d1b45fb46aad544698c73ec00ddb58db
David Souza <dabcs@poli.ufrj.br>
2022-06-09 15:00:30 -0300

    fix: subindo ultimas alteracoes

--- front-end/src/pages/CheckoutPage.jsx
+++ front-end/src/pages/CheckoutPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/navbar/Header';
 
 function CheckoutPage() {
   return (
     <div>
-      CheckoutPage
+      <Header />
     </div>
   );
 }
